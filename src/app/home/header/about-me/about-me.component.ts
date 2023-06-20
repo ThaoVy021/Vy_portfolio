57b65b1acda9545759a04df5953453d133296d9b
thao vy <lttvy2@cmcglobal.vn>
2023-06-20 15:42:33 +0700

    fix button contact scroll to bottom

--- src/app/home/header/about-me/about-me.component.ts
+++ src/app/home/header/about-me/about-me.component.ts
@@ -1,8 +1,15 @@
-import { Component, ViewChild } from '@angular/core'
+import { Component, HostListener } from '@angular/core'
 
 @Component({
   selector: 'portfolio-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss'],
 })
-export class AboutMeComponent {}
+export class AboutMeComponent {
+  scrollTo(elementId: string) {
+    window.scrollTo({
+      top: document.getElementById(elementId)?.offsetTop,
+      behavior: 'smooth',
+    })
+  }
+}
